852ae67751caae41ed7764e3df880dba7872941d
Pritam Mandal <2305554@kiit.ac.in>
2026-03-05 19:43:24 +0530

    security: reduce JWT cookie lifetime from 7 days to 1 day

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -31,7 +31,7 @@ export async function loginAdmin(req: Request, res: Response) {
       httpOnly: true,
       secure: isProduction,
       sameSite,
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      maxAge: 1 * 24 * 60 * 60 * 1000 // 1 day
     };
 
     // Set HttpOnly cookie
